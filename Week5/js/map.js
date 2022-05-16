89dd555a0f68fd3e5220de365fb955063eaad39c
Cynthia Gong <cynthiagong@g.ucla.edu>
2022-05-15 17:48:32 -0700

    chloropleth edits

--- Week5/js/map.js
+++ Week5/js/map.js
@@ -5,6 +5,13 @@ let lon = 0;
 let zl = 1;
 let globaldata;
 
+let geojsonPath = 'data/nyc-boroughs.json';
+let geojson_data;
+let geojson_layer;
+let brew = new classyBrew();
+let legend = L.control({position: 'bottomright'});
+let info_panel = L.control();
+
 // path to csv data
 
 let path = "data/arrest.csv";
@@ -20,7 +27,8 @@ let whitehispanicmarkers = L.featureGroup();
 // initialize
 $( document ).ready(function() {
     createMap(lat,lon,zl);
-    readCSV(path);
+	getGeoJSON();
+	readCSV(path);
 });
 
 // create the map
@@ -77,8 +85,7 @@ function readCSV(path){
 		}
 	});
 }
-//add another argument for color to differentiate for different groups) 
-//color, name/title
+
 function mapCSV(data, featuregroup, color, name){
 	
 	// circle options
@@ -107,8 +114,152 @@ function mapCSV(data, featuregroup, color, name){
 
 	// fit markers to map
 	map.fitBounds(featuregroup.getBounds())
+}
 
-	//layers[name] = featuregroup;
+// function to get the geojson data
+function getGeoJSON(){
+
+	$.getJSON(geojsonPath,function(data){
+		console.log(data)
+
+		// put the data in a global variable
+		geojson_data = data;
+
+		// call the map function
+		mapGeoJSON('asian') // add a field to be used
+	})
+}
+
+function mapGeoJSON(field){
+
+	// clear layers in case it has been mapped already
+	if (geojson_layer){
+		geojson_layer.clearLayers()
+	}
 	
-	//L.control.layers(null, layers).addTo(map)
+	// globalize the field to map
+	fieldtomap = field;
+
+	// create an empty array
+	let values = [];
+
+	// based on the provided field, enter each value into the array
+	geojson_data.features.forEach(function(item,index){
+		values.push(item.properties[field])
+	})
+
+	// set up the "brew" options
+	brew.setSeries(values);
+	brew.setNumClasses(5);
+	brew.setColorCode('YlOrRd');
+	brew.classify('equal_interval');
+
+	// create the layer and add to map
+	geojson_layer = L.geoJson(geojson_data, {
+		style: getStyle, //call a function to style each feature
+		onEachFeature: onEachFeature // actions on each feature
+	}).addTo(map);
+
+	map.fitBounds(geojson_layer.getBounds());
+
+	createLegend();
+
+	createInfoPanel();
+}
+
+function getStyle(feature){
+	return {
+		stroke: true,
+		color: 'white',
+		weight: 1,
+		fill: true,
+		fillColor: brew.getColorInRange(feature.properties[fieldtomap]),
+		fillOpacity: 0.8
+	}
+}
+
+function createLegend(){
+	legend.onAdd = function (map) {
+		var div = L.DomUtil.create('div', 'info legend'),
+		breaks = brew.getBreaks(),
+		labels = [],
+		from, to;
+		
+		for (var i = 0; i < breaks.length; i++) {
+			from = breaks[i];
+			to = breaks[i + 1];
+			if(to) {
+				labels.push(
+					'<i style="background:' + brew.getColorInRange(from) + '"></i> ' +
+					from.toFixed(2) + ' &ndash; ' + to.toFixed(2));
+				}
+			}
+			
+			div.innerHTML = labels.join('<br>');
+			return div;
+		};
+		
+		legend.addTo(map);
+}
+
+// Function that defines what will happen on user interactions with each feature
+function onEachFeature(feature, layer) {
+	layer.on({
+		mouseover: highlightFeature,
+		mouseout: resetHighlight,
+		click: zoomToFeature
+	});
+}
+
+// on mouse over, highlight the feature
+function highlightFeature(e) {
+	var layer = e.target;
+
+	// style to use on mouse over
+	layer.setStyle({
+		weight: 2,
+		color: '#666',
+		fillOpacity: 0.7
+	});
+
+	// if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+	// 	layer.bringToFront();
+	// }
+
+	info_panel.update(layer.feature.properties)
+}
+
+// on mouse out, reset the style, otherwise, it will remain highlighted
+function resetHighlight(e) {
+	geojson_layer.resetStyle(e.target);
+	info_panel.update() // resets infopanel
+}
+
+// on mouse click on a feature, zoom in to it
+function zoomToFeature(e) {
+	map.fitBounds(e.target.getBounds());
+}
+
+function createInfoPanel(){
+
+	info_panel.onAdd = function (map) {
+		this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
+		this.update();
+		return this._div;
+	};
+
+	// method that we will use to update the control based on feature properties passed
+	info_panel.update = function (properties) {
+		// if feature is highlighted
+		if(properties){
+			this._div.innerHTML = `<b>${properties.name}</b><br>${fieldtomap}: ${properties[fieldtomap]}`;
+		}
+		// if feature is not highlighted
+		else
+		{
+			this._div.innerHTML = 'Hover over a borough';
+		}
+	};
+
+	info_panel.addTo(map);
 }
